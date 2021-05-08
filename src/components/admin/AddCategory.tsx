73c16095c47364ce0157895df2aa5efc3659bee0
comedyOfKing <f_hhong@163.com>
2021-05-08 17:47:35 +0800

    addcategory luoji

--- src/components/admin/AddCategory.tsx
+++ src/components/admin/AddCategory.tsx
@@ -1,10 +1,33 @@
-import { Button, Form, Input } from 'antd'
-import React from 'react'
+import { Button, Form, Input, message } from 'antd'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { API } from '../../config'
+import { isAuth } from '../../helpers/auth'
+import { Jwt } from '../../store/models/auth'
 import Layout from '../core/Layout'
 const Item = Form.Item
 const AddCategory = () => {
-  const onFinish = (val: { name: string}) => {
+  const [name, setName] = useState<string>('')
+  const { user, token } = isAuth() as Jwt
 
+  useEffect(() => {
+    async function addCategory() {
+      try {
+        let res = await axios.post<{name: string}>(`${API}/category/create/${user._id}`, {name}, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        message.success(`${res.data.name}添加成功`, 1)
+      } catch (error) {
+        message.error(error.response.data.error, 1)
+      }
+    }
+    name && addCategory()
+  }, [name, token, user])
+  const onFinish = (val: { name: string}) => {
+    console.log(val)
+    setName(val.name)
   }
   return (
     <Layout title="添加分类" subTitle="">

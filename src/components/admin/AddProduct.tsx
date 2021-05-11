949b1af18586d9a0bb200fb4de35b3209dc191c9
comedyOfKing <f_hhong@163.com>
2021-05-11 20:27:47 +0800

    add product

--- src/components/admin/AddProduct.tsx
+++ src/components/admin/AddProduct.tsx
@@ -1,26 +1,56 @@
-import { Button, Form, Input, Select, Upload } from 'antd'
-import { useEffect } from 'react'
+import { Button, Form, Input, message, Select, Upload } from 'antd'
+import { useEffect, useState } from 'react'
 import Layout from '../core/Layout'
 import { UploadOutlined } from '@ant-design/icons'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCategory } from '../../store/actions/category.actions'
 import { AppState } from '../../store/reduces'
 import { CategoryState } from '../../store/reduces/category.reducer'
+import { RcFile } from 'antd/lib/upload'
+import axios from 'axios'
+import { API } from '../../config'
+import { isAuth } from '../../helpers/auth'
+import { Jwt } from '../../store/models/auth'
 const Item = Form.Item
+const { Option } = Select;
 const AddProducts = () => {
   const dispatch = useDispatch()
   const category = useSelector<AppState, CategoryState>(state => state.category)
-
+  const [file, setFile] = useState<RcFile>()
   useEffect(() => {
     dispatch(getCategory())
   }, [dispatch])
-  const onFinish = (val: { name: string}) => {
+  const { user, token } = isAuth() as Jwt
+  const onFinish = (product: any) => {
+    const formDate = new FormData()
+    for(let attr in product) {
+      formDate.set(attr, product[attr])
+    }
+    if (typeof file !== "undefined") {
+      formDate.set("photo", file)
+    }
+    axios.post(`${API}/product/create/${user._id}`, formDate, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(res => {
+      message.success('商品添加成功')
+    }).catch(e => {
+      message.error('商品添加失败')
+    })
   }
-  return (
-    <Layout title="添加商品" subTitle="">
-      <Form initialValues={{category: ''}} onFinish={onFinish}>
-        <Item name="name" label="商品名称">
-          <Upload>
+  const addProductForm = () => {
+    const props = {
+      accepts: "image/jpeg/png/*",
+      beforeUpload: function(file: RcFile) {
+        setFile(file)
+        return false
+      }
+    }
+    return (
+      <Form initialValues={{category: '60965d06e9e30e474c6a1929', shipping: '1'}} onFinish={onFinish}>
+        <Item >
+          <Upload {...props}>
             <Button icon={<UploadOutlined />}>上传商品封面</Button>
           </Upload>
         </Item>
@@ -35,7 +65,7 @@ const AddProducts = () => {
         </Item>
         <Item name="category" label="所属分类">
           <Select>
-            <Select.Option value="">请选择分类</Select.Option>
+            <Option value="">请选择分类</Option>
             {
               category.category.result.map((item) => {
                 return (
@@ -49,7 +79,10 @@ const AddProducts = () => {
           <Input></Input>
         </Item>
         <Item name="shipping" label="是否需要运输">
-          <Input></Input>
+          <Select>
+            <Option value="1">是</Option>
+            <Option value="0">否</Option>
+          </Select>
         </Item>
         <Item >
           <Button type="primary" htmlType="submit">
@@ -57,6 +90,11 @@ const AddProducts = () => {
           </Button>
         </Item>
       </Form>
+    )
+  }
+  return (
+    <Layout title="添加商品" subTitle="">
+      {addProductForm()}
     </Layout>
   )
 }
